0c08fbea28d71e5b73a2cf29c73708690a7d374f
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2023-06-27 04:40:41 -0300

    added movies template

--- pages/movies.vue
+++ pages/movies.vue
@@ -1,21 +1,30 @@
 <template>
-    <div>
-        Movies
-    </div>
+    <div>Movies</div>
 </template>
 
 <script>
 import Cookies from 'js-cookie'
 export default {
-  name: 'Movies',
+  name: 'Index',
   mounted () {
-    
+    this.checkToken()
+    this.getMovies({t: 'a', y: '2023'})
+  },
+  data () {
+    return {
+        token: ''
+    }
   },
   methods: {
-    
+    checkToken () {
+        this.token = Cookies.get('token')
+        if (!this.token) this.$router.push('/login')
+    },
+    getMoviesbyTitle () {
+    },
+    searchMovies () {
+        
+    }
   }
 }
 </script>
-
-<style>
-</style>
